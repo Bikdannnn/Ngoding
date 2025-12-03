862623f0e2307e9f1fe70da29db5117c6670f2ad
M. Wildan Humaidi S. Budi <wildanhumaidi5579@gmail.com>
2025-12-03 21:57:39 +0800

    Belajar lagi

---
+++ Belajar/belajarps.py
@@ -0,0 +1,13 @@
+wildan = [1, 2, 3, 4, 5]
+
+wildan.append(6)
+
+wildan.sort()
+wildan.remove(3)
+
+
+print(wildan)
+print("Panjang list:", len(wildan))
+print("Elemen terbesar:", max(wildan))
+print("Elemen terkecil:", min(wildan))
+print("Jumlah semua elemen:", sum(wildan))
