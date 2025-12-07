595183f28c3e8206d8acaa1f91874cfc2556e92f
M. Wildan Humaidi S. Budi <wildanhumaidi5579@gmail.com>
2025-12-07 23:21:25 +0800

    blajarblajar

--- Belajar/belajarps.py
+++ Belajar/belajarps.py
@@ -1,4 +1,4 @@
-wildan = [1, 2, 3, 4, 5]
+wildan = [1, 2, 3, 4, 5, 9, 8, 7]
 
 wildan.append(6)
 
@@ -13,3 +13,16 @@ print("Elemen terkecil:", min(wildan))
 print("Jumlah semua elemen:", sum(wildan))
 
 print([d for d in wildan if d > 2])
+
+print([n for n in wildan if n % 2 == 0])
+print([n for n in wildan if n % 2 != 0])
+
+nilaimax = max(wildan)
+nilaimin = min(wildan)
+sorted_wildan = sorted(wildan)
+
+print("Nilai terbesar:", nilaimax)
+print("Nilai terkecil:", nilaimin) 
+print("List terurut:", sorted_wildan)
+print("List terbalik:", list(reversed(wildan)))
+print("List isi wildan:", wildan)
