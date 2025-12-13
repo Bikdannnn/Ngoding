eb2466af33d83ee6cb4bf4bf6758b120482abe08
M. Wildan Humaidi S. Budi <wildanhumaidi5579@gmail.com>
2025-12-13 13:35:32 +0800

    rapikanpemscript

--- Kulyah/Pemsrcpt/D121241058_MWildanHumaidiSBudi_soal1.py
+++
@@ -1,9 +0,0 @@
-#soal-1
-
-angka = [3, 10, 7, 8, 15, 22, 1]
-
-genap = [n for n in angka if n % 2 == 0]
-ganjil = [n for n in angka if n % 2 != 0]
-
-print("Genap:", genap)
-print("Ganjil:", ganjil)
