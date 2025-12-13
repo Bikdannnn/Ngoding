65e796e4f4a49aa0fafe0b76cde5188446e1ab6f
M. Wildan Humaidi S. Budi <wildanhumaidi5579@gmail.com>
2025-12-13 13:35:55 +0800

    rapikanoop

--- Kulyah/OOP/LoopTest.java
+++
@@ -1,9 +0,0 @@
-public class LoopTest {
-    public static void main(String[] args) {
-        int i = 0;
-        while (i < 3) {
-            System.out.print(i + " ");
-            i++;
-        }
-    }
-}
