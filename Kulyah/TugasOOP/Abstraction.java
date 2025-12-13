92c39c18128687cfd2510520205f456b95012e64
M. Wildan Humaidi S. Budi <wildanhumaidi5579@gmail.com>
2025-12-13 13:34:51 +0800

    rapikantugasoop

--- Kulyah/TugasOOP/Abstraction.java
+++
@@ -1,9 +0,0 @@
-public class Abstraction {
-    public static void main(String[] args) {
-        
-        Lingkaran lingkaranMerah = new Lingkaran("Merah", 7.0);
-
-        lingkaranMerah.tampilWarna();
-        System.out.println("Luas lingkaran: " + lingkaranMerah.hitungLuas());
-    }
-}
