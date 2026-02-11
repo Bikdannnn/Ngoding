866864be5b51050e27d85e114fd5f33f79a321ee
M. Wildan Humaidi S. Budi <wildanhumaidi5579@gmail.com>
2026-02-11 14:14:29 +0800

    readline v2

--- readline.js
+++ readline.js
@@ -3,9 +3,16 @@ const { stdin: input, stdout: output } = require('node:process');
 
 const rl = readline.createInterface({ input, output });
 
-rl.question('siapa namamu?: ', (answer) => {
-rl.question(`dimana kamu tinggal ${answer}? :`, (answer2) => {
-    console.log(`Thank you ${answer} yang tinggal di ${answer2}`)});
+// rl.question('siapa namamu?: ', (answer) => {
+//     console.log(`Thank you ${answer}`);
+// rl.question(`dimana kamu tinggal ${answer}? :`, (answer2) => {
+//     console.log(`oh jadi kamu tinggal di ${answer2}`);
+//   rl.close();
+// })});
 
-  rl.close();
+rl.question('siapa namamu?: ', (nama) => {
+    rl.question(`dimana kamu tinggal ${nama}? :`, (alamat) => {
+        console.log(`Halo ${nama}, jadi kamu tinggal di ${alamat}`);
+        rl.close();
+    });
 });
