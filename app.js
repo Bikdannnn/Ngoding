0fd5de71fbea87f2eeb8edf30a665018be3ef36b
M. Wildan Humaidi S. Budi <wildanhumaidi5579@gmail.com>
2026-02-11 13:50:51 +0800

    write & read file node js

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const fs = require('fs');
 
-const data = fs.writeFile('data/text.txt', 'Hello World', (e) => {
-    console/log(e)})
+// const data = fs.writeFile('data/text.txt', 'Hello World', (e) => {
+//     console/log(e)})
 
-console.log(data)
+// console.log(data)
+
+fs.readFile('data/text.txt', 'utf-8', (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+})
