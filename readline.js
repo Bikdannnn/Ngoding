c3b37cfe143a8bd4f721ceecc81523505a4429c3
M. Wildan Humaidi S. Budi <wildanhumaidi5579@gmail.com>
2026-02-12 13:19:59 +0800

    rev lagi

--- readline.js
+++ readline.js
@@ -21,8 +21,46 @@ if (!fs.existsSync(filePath)) {
     fs.writeFileSync(filePath, '[]', 'utf-8');
 }
 
+const pertanyaan1 = () => {
+    return new Promise((resolve, reject) => {
+        rl.question('siapa namamu?: ', (nama) => {
+            resolve(nama);
+        });
+    });
+};
+
+const pertanyaan2 = () => {
+    return new Promise((resolve, reject) => {
+        rl.question('berapa nomor teleponmu?: ', (noHp) => {
+            resolve(noHp);
+        });
+    });
+};
+
+const pertanyaan3 = () => {
+    return new Promise((resolve, reject) => {
+        rl.question('masukkan email kamu?: ', (email) => {
+            resolve(email);
+        });
+    });
+};
+
+const main = async () => {
+    const nama = await pertanyaan1();
+    const noHp = await pertanyaan2();
+    const email = await pertanyaan3();
 
 
+     const contact = { nama, noHp, email };
+        const file = fs.readFileSync(filePath, 'utf-8');
+        const contacts = JSON.parse(file);
+        contacts.push(contact);
+        fs.writeFileSync(filePath, JSON.stringify(contacts));
+        console.log('terimakasih sudah memasukkan data');
+    rl.close();
+};
+
+main();
 // rl.question('siapa namamu?: ', (nama) => {
 //     rl.question('berapa nomor teleponmu?: ', (noHp) => {
 //         const contact = { nama, noHp };
