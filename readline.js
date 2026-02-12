46482fbcabcdcc8d7f69770f370ca4a89f8e2dd8
M. Wildan Humaidi S. Budi <wildanhumaidi5579@gmail.com>
2026-02-12 13:02:36 +0800

    sedikit revisi untuk app contact

---
+++ readline.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
+
+const rl = readline.createInterface({ input, output });
+
+// rl.question('siapa namamu?: ', (answer) => {
+//     console.log(`Thank you ${answer}`);
+// rl.question(`dimana kamu tinggal ${answer}? :`, (answer2) => {
+//     console.log(`oh jadi kamu tinggal di ${answer2}`);
+//   rl.close();
+// })});
+
+const dirPath = './data';
+if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+}
+
+const filePath = './data/contacts.json';
+if (!fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, '[]', 'utf-8');
+}
+
+rl.question('siapa namamu?: ', (nama) => {
+    rl.question('berapa nomor teleponmu?: ', (noHp) => {
+        const contact = { nama, noHp };
+        const file = fs.readFileSync(filePath, 'utf-8');
+        const contacts = JSON.parse(file);
+        contacts.push(contact);
+        fs.writeFileSync(filePath, JSON.stringify(contacts));
+        console.log('terimakasih sudah memasukkan data');
+        rl.close();
+    });
+});
