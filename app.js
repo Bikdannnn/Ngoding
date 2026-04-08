9eb2b74bac4fbbf5ac3f22f90bb391cc76b2ff13
M. Wildan Humaidi S. Budi <wildanhumaidi5579@gmail.com>
2026-04-08 20:55:21 +0800

    janglan lupa dailyy

--- app.js
+++ app.js
@@ -78,6 +78,9 @@ app.get('/', (req, res) => {
         },
         { nama: 'madun', 
           email: 'madun@gmail.com',
+        },
+        { nama: 'mesi', 
+          email: 'mesi@gmail.com',
         }
     ]
     
