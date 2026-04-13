ec302c59b17f330111f1e69fac391208fa4df134
M. Wildan Humaidi S. Budi <wildanhumaidi5579@gmail.com>
2026-04-13 16:31:58 +0800

    13 april

--- app.js
+++ app.js
@@ -78,6 +78,9 @@ app.get('/', (req, res) => {
         },
         { nama: 'mesi', 
           email: 'mesi@gmail.com',
+        },
+        { nama: 'adibz', 
+          email: 'adibz@gmail.com',
         }
     ]
     
